eca86b18c22e091598ab016b6013fcf63a39857f
Harry Shoemaker <hshoemaker@istation.com>
2026-06-19 15:00:27 -0500

    update stats and add intial looksups

--- src/app/models/character.ts
+++ src/app/models/character.ts
@@ -1,26 +1,31 @@
 export interface Character {
   name: string;
+  title: string;
   isCrawler: boolean;
   crawlerNumber: number | undefined;
   race: string;
   class: string | undefined;
   level: number | undefined;
   crawlerStats: CrawlerStats | undefined;
-  socialStats: CrawlerSocialStats | undefined;
+  socialStats: CrawlerRatings | undefined;
   skills: Skill[] | undefined;
   inventory: Item[] | undefined;
+  crawlerKillerCount: number | undefined;
 }
 
 export interface CrawlerStats {
   strength: number | undefined;
   constitution: number | undefined;
   intellegence: number | undefined;
+  dexterity: number | undefined;
+  charisma: number | undefined;
 }
 
-export interface CrawlerSocialStats {
+export interface CrawlerRatings {
   views: number;
   followers: number;
   favorits: number;
+  patrons: number;
 }
 
 export interface Skill {
@@ -34,4 +39,20 @@ export interface Item {
   type: string;
   rarity: string;
   equiped: string;
+}
+
+export interface Buff {
+  name: string;
+  description: string;
+}
+
+export interface Spell {
+  name: string;
+  description: string;
+  level: number;
+}
+
+export interface Party {
+  name: string;
+  members: Character[];
 }
